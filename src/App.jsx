1221bcffed1f63d17d0ae8dcba2101501a344ba8
Coded-Adil <itsadil2022@gmail.com>
2024-10-28 01:26:49 +0600

    Addad navbar and a toogle feature to the nav burger button

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import NavBar from './components/Nav/NavBar'
+import Navbar from './components/Navbar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <Navbar></Navbar>
+      <h1 className='text-7xl'>Vite + React</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav/NavBar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import Link from "../Link/Link";
+import { IoMenu } from "react-icons/io5";
+import { GrClose } from "react-icons/gr";
+
+
+const NavBar = () => {
+    const [open, setOpen] = useState(false);
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/services', name: 'Services' },
+        { id: 4, path: '/contact', name: 'Contact' },
+        { id: 5, path: '/profile/:username', name: 'Profile' }
+    ];
+
+    return (
+        <nav>
+            <div className="md:hidden" onClick={() => setOpen(!open)}>
+                {
+                    open === true ? <GrClose className="text-3xl"></GrClose> : <IoMenu className="text-3xl"></IoMenu>
+                }
+            </div>
+            <ul className="md:flex">
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
